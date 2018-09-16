a6a29d3ca3e115d99d5a3148624dfaadcaeac1ff
Tong Sun <suntong@cpan.org>
2018-09-16 00:57:17 -0400

    - [*] update tests

--- test/test-sym.sh
+++ test/test-sym.sh
@@ -1,8 +1,11 @@
 #! /bin/sh
 
 # Test -sym control
-../ffcvt -n -d . -debug 1 > /tmp/ffcvt_test.txt 2>&1 
-../ffcvt -n -d . -debug 1 -sym  >> /tmp/ffcvt_test.txt 2>&1 
+../ffcvt -t x265-opus -n -d . > /tmp/ffcvt_test.txt 2>&1
+../ffcvt -t x265-opus -n -d . -sym  >> /tmp/ffcvt_test.txt 2>&1
+
+../ffcvt -n -d .  >> /tmp/ffcvt_test.txt 2>&1
+../ffcvt -n -d . -sym  >> /tmp/ffcvt_test.txt 2>&1
 
 sed -i '/ [0-9.]*[mµ]*s$/d' /tmp/ffcvt_test.txt
 diff -wU 1 /tmp/ffcvt_test.txt .
