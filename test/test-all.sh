33550ce1771bd1cb467a22fe13067fad36f16553
Tong Sun <suntong@cpan.org>
2019-12-30 22:39:59 -0500

    - [+] add testing for `go generate`

--- test/test-all.sh
+++ test/test-all.sh
@@ -2,10 +2,13 @@
 
 ../ffcvt -version
 
-# Test transcoding single file
-../ffcvt -n -debug 0 -f StreamSample.mkv -w /tmp > /tmp/ffcvt_test.txt 2>&1
+echo 'Test (config.go) cli help output'
+../ffcvt > /tmp/ffcvt_test.txt 2>&1
 
-# Test -sym control
+echo Test transcoding single file
+../ffcvt -n -debug 0 -f StreamSample.mkv -w /tmp >> /tmp/ffcvt_test.txt 2>&1
+
+echo Test -sym control
 ../ffcvt -t x265-opus -n -d . >> /tmp/ffcvt_test.txt 2>&1
 ../ffcvt -t x265-opus -n -d . -sym  >> /tmp/ffcvt_test.txt 2>&1
 
@@ -14,6 +17,7 @@
 
 ../ffcvt -n -sym -debug 2 -d . -w /tmp >> /tmp/ffcvt_test.txt 2>&1
 
+echo Compare test results
 sed -i '/ [0-9.]*[mµ]*s$/d' /tmp/ffcvt_test.txt
 diff -wU 1 ffcvt_test.txt /tmp/ffcvt_test.txt
 
