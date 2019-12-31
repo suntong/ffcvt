24af3f8ce63cb3c9b4cc8401f3f93e1b522b22cb
Tong Sun <suntong@cpan.org>
2019-12-31 17:38:38 -0500

    - [+] add more template file location

--- ffcvt_cli.sh
+++ ffcvt_cli.sh
@@ -1,5 +1,6 @@
 templateFile=$GOPATH/src/github.com/go-easygen/easygen/test/commandlineFlag
 [ -s $templateFile.tmpl ] || templateFile=/usr/share/gocode/src/github.com/go-easygen/easygen/test/commandlineFlag
+[ -s $templateFile.tmpl ] || templateFile=/usr/share/doc/easygen/examples/commandlineFlag
 [ -s $templateFile.tmpl ] || {
   echo No template file found
   exit 1
